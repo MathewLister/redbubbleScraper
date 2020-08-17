febb887ef3e92c5fd2c9fe19221461f18791750c
MathewLister <listermathew0@gmail.com>
2020-08-16 17:06:06 -0700

    Linting

--- web_scraper/design_scraper.py
+++ web_scraper/design_scraper.py
@@ -1,49 +1,41 @@
-#This script should gather all design metadata
-from urllib.request import urlopen
-from bs4 import BeautifulSoup 
+# This script should gather all design metadata
+from bs4 import BeautifulSoup
 from selenium import webdriver
-
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-import time
-import sys
 
-#Using a headless chrome
+
+# Using a headless chrome
 PATH = "./chromedriver.exe"
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=PATH, options=chrome_options)
 
-#URL to the explore designs page
+# URL to the explore designs page
 url_to_scrape = "https://www.redbubble.com/people/S0ULGROUPSTUDIO/explore"
-#Open the page
+# Open the page
 driver.get(url_to_scrape)
-#Read page
+# Read page
 html = driver.page_source
-#Close the page
+# Close the page
 driver.quit()
 
-#Parse the html
+# Parse the html
 html_soup = BeautifulSoup(html, 'html.parser')
 
-#Target data 
+# Target data
 design_card_links = html_soup.findAll("a", class_="styles__link--2sYi3")
-#design_card_images = html_soup.findAll("img", class_="styles__image--2CwxX styles__rounded--1lyoH styles__fluid--3dxe-")
+# design_card_images = html_soup.findAll("img", class_="styles__image--2CwxX styles__rounded--1lyoH styles__fluid--3dxe-")
 design_card_names = html_soup.findAll("span", class_="styles__box--206r9 styles__text--NLf2i styles__display6--uq_8G styles__nowrap--2Vk3A styles__display-block--2XANJ ")
 
-
-#URL
+# URL
 for link in design_card_links:
-    #Design URL
+    # Design URL
     design_url = link.get('href')
     print("URL: ", design_url)
-#Image source
+# Image source
 '''for image in design_card_images:
     design_photo_url = image.get('src')
     print("Photo URL: ", image)'''
-#Design name    
+# Design name
 for name in design_card_names:
     print("Design Name: ", name.contents)
